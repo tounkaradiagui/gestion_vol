25fc0965abfbdd97bdac4a6241fff950ed69a660
tounkaradiagui <tounkaradiagui@gmail.com>
2022-07-25 19:16:57 +0200

    'essaiee'

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['verify' => true]);
+Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
