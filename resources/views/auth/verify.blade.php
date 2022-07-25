b10c481a091232b6636aea96e0c49bcce8f7e807
tounkaradiagui <tounkaradiagui@gmail.com>
2022-07-25 21:43:21 +0200

    'ddd'

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,83 +1,28 @@
-@extends('layouts.master')
-
-@section('title', 'Tableau de bord Admin')
+@extends('layouts.app')
 
 @section('content')
-<div class="container-fluid px-4">
-    <h1 class="mt-4">Dashboard</h1>
-    <div class="row">
-        <div class="col-xl-3 col-md-6">
-            <div class="card bg-success text-white mb-4">
-                <div class="card-body">Total Admin
-                <h2>{{$admins}}</h2>
-                </div>
-                <div class="card-footer d-flex align-items-center justify-content-between">
-                    <a class="small text-white stretched-link" href="{{url('admin/users')}}">Plus de Détails</a>
-                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3 col-md-6">
-            <div class="card bg-primary text-white mb-4">
-                <div class="card-body">Reservation Total
-                    <h2>{{$reservations}}</h2>
-                </div>
-                <div class="card-footer d-flex align-items-center justify-content-between">
-                    <a class="small text-white stretched-link" href="{{url('admin/reservation')}}">Plus de Détails</a>
-                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3 col-md-6">
-            <div class="card bg-warning text-white mb-4">
-                <div class="card-body">Nombre de vols
-                <h2>{{$vols}}</h2>
-                </div>
-                <div class="card-footer d-flex align-items-center justify-content-between">
-                    <a class="small text-white stretched-link" href="{{url('admin/vol')}}">Plus de Détails</a>
-                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3 col-md-6">
-            <div class="card bg-info text-white mb-4">
-                <div class="card-body">Users
-                <h2>{{$users}}</h2>
-                </div>
-                <div class="card-footer d-flex align-items-center justify-content-between">
-                    <a class="small text-white stretched-link" href="{{url('admin/users')}}">Plus de Détails</a>
-                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Verifer votre Adresse Email ') }}</div>
+
+                <div class="card-body">
+                    @if (session('resent'))
+                        <div class="alert alert-success" role="alert">
+                            {{ __('Un message de verification avec un lien a été envoyé à votre adresse email .') }}
+                        </div>
+                    @endif
+
+                    {{ __('Avant de proceder, merci de consulter votre email pour vérifier le lien.') }}
+                    {{ __('Si vous navez pas reçu le lien par email') }},
+                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                        @csrf
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('Cliquez ici pour demander un autre') }}</button>.
+                    </form>
                 </div>
             </div>
         </div>
     </div>
-    <!-- ###################################################################### -->
-
-    <div class="row">
-        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item ">
-                    <img src="{{asset('assets/images/B.jpg')}}" class="d-block w-100" height="500" width="100" alt="...">
-                </div>
-                <div class="carousel-item ">
-                    <img src="{{asset('assets/images/C.jpg')}}" class="d-block w-100" height="500" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="{{asset('assets/images/D.jpg')}}" class="d-block w-100" height="500" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="{{asset('assets/images/E.jpg')}}" class="d-block w-100" height="500" alt="...">
-                </div>
-                <div class="carousel-item active">
-                    <img src="{{asset('assets/images/F.jpg')}}" class="d-block w-100" height="500" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="{{asset('assets/images/G.jpg')}}" class="d-block w-100" height="500" alt="...">
-                </div>
-            </div>
-        </div>
-    </div>
-
 </div>
-
 @endsection
