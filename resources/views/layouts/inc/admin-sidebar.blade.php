6f46e9324c25ac024fbaa303fb8d5fa37572000c
diagui <tounkarakan@gmail.com>
2022-09-22 11:08:41 +0200

    ok

--- resources/views/layouts/inc/admin-sidebar.blade.php
+++ resources/views/layouts/inc/admin-sidebar.blade.php
@@ -1,8 +1,8 @@
 <div id="layoutSidenav_nav">
-    <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+    <nav class="sb-sidenav accordion bg-light" id="sidenavAccordion">
         <div class="sb-sidenav-menu">
             <div class="nav">
-                <div class="sb-sidenav-menu-heading">Core</div>
+                <!-- <div class="sb-sidenav-menu-heading">Dashboard</div> -->
                 <a class="nav-link" href="{{ url('admin/dashboard')}}">
                     <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                     Accueil
